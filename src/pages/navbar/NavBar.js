b2683754f46c8c3992412b5c3eab27dec8dcc3cb
dustin1709 <duong.chu.vu@lalumiere.org>
2022-01-26 22:48:05 -0500

    fix navbar to make it responsive

--- src/pages/navbar/NavBar.js
+++ src/pages/navbar/NavBar.js
@@ -1,43 +1,58 @@
-import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { NavLink, Link } from "react-router-dom";
 import logo from './logo.png';
+import * as FaIcons from 'react-icons/fa';
 
 const NavBar = () => {
+
+    const [isMobile, setMobile] = useState(window.innerWidth < 699);
+      
+    const updateMedia = () => {
+        setMobile(window.innerWidth < 699);
+    };
+      
+    useEffect(() => {
+        window.addEventListener("resize", updateMedia);
+        return () => window.removeEventListener("resize", updateMedia);
+    });
+
     return (
-        <div className="d-flex flex-column flex-shrink-0 p-3 text-white MyNavBar" style={{backgroundColor: '#F29441'}}>
-            <img src={logo} alt="Logo" width="55%" />
-            <hr/>
+        <div className='d-flex flex-column flex-shrink-0 p-3 text-white MyNavBar' style={{backgroundColor: '#F29441'}}>
+            <img src={logo} alt="Logo" width="55%" className={isMobile ? 'HideThisOne' :  ''} />
+            <hr className={isMobile ? 'HideThisOne' :  ''}/>
             <ul className="nav nav-pills flex-column mb-auto">
                 <li>
                     <NavLink to="/dashboard" activeclassname="active" className="nav-link text-white">
-                        Dashboard
+                        {isMobile ? FaIcons.FaHome :  'Dashboard'}
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to="/search" activeclassname="active" className="nav-link text-white">
-                        Search
+                        {isMobile ? FaIcons.FaSearch :  'Search'}
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to="/reservation" activeclassname="active" className="nav-link text-white">
-                        Reservation
+                        {isMobile ? FaIcons.FaCalendar :  'Reservation'}
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to="/cart" activeclassname="active" className="nav-link text-white">
-                        Shopping Cart
+                        {isMobile ? FaIcons.FaShoppingCart :  'Shopping Cart'}
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to="/kit" activeclassname="active" className="nav-link text-white">
-                        Kit
+                        {isMobile ? FaIcons.FaBriefcase :  'Kit'}
                     </NavLink>
                 </li>
-
-                <li class="mb-1">
-                    <button style={{position: 'absolute', bottom: '0', marginBottom: '3%', marginLeft: '5.5%'}} 
+                <div style={{padding: '5%', clear: 'both'}}></div>
+                <hr className={isMobile ? 'HideThisOne' :  ''}/>
+                <li>
+                    <button 
                     type="button" 
-                    className="btn btn-light">
-                        Log out
+                    className="btn">
+                        <FaIcons.FaSignOutAlt /> {isMobile ? '' :  'Sign Out'}
                     </button>
                 </li>
             </ul>
