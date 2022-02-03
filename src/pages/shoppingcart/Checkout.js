d86d85bc332b743fa1a82d0d39499f6101bf8216
jing899 <sxw6348@g.rit.edu>
2022-02-03 15:37:16 -0500

    add checkout page

---
+++ src/pages/shoppingcart/Checkout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../navbar/NavBar";
+
+const Checkout = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="mainContainerRight"></div>
+    </>
+  );
+};
+
+export default Checkout;
