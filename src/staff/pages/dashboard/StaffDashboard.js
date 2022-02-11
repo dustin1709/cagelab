c131d4845eed1f608ec263c14c7945a2e8c0e484
dustin1709 <duong.chu.vu@lalumiere.org>
2022-02-10 21:16:20 -0500

    staff dashboard

--- src/staff/pages/dashboard/StaffDashboard.js
+++ src/staff/pages/dashboard/StaffDashboard.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { useState, useEffect } from "react";
 import StaffNavBar from "../../components/StaffNavBar";
+import "./dashboard.css";
 
 const StaffDashboard = () => {
 
@@ -17,7 +18,62 @@ const StaffDashboard = () => {
     <>
       <StaffNavBar />
       <div className="mainContainerRight">
-        
+        <div className="page-row">
+          <div className="DButton">
+            <Link to="/staff/search" className="nav-link text-black">
+              Search Item
+            </Link>
+          </div>
+          <div className="DButton">
+            <Link to="/staff/checkin" className="nav-link text-black">
+              Check in
+            </Link>
+          </div>
+          <div className="DButton">
+            <Link to="/staff/cart" className="nav-link text-black">
+              Shopping Cart
+            </Link>
+          </div>
+        </div>
+
+        <div className="page-row">
+          <div className="dashboard-comp">
+            <h4>Reservation</h4>
+            <ul>
+
+            </ul>
+            <Link to="/staff/reservation">
+              <Button variant="secondary">View Details</Button>{" "}
+            </Link>
+          </div>
+          <div className="dashboard-comp">
+            <h4>Kit Request</h4>
+            <ul>
+
+            </ul>
+            <Link to="/staff/kit">
+              <Button variant="secondary">View Details</Button>{" "}
+            </Link>
+          </div>
+        </div>
+
+        <div className="page-row">
+          <div className="DButton">
+            <Link to="/staff/inventory" className="nav-link text-black">
+              Iventory
+            </Link>
+          </div>
+          <div className="DButton">
+            <Link to="/staff/report" className="nav-link text-black">
+              Report
+            </Link>
+          </div>
+          <div className="DButton">
+            <Link to="/staff/settings" className="nav-link text-black">
+              Settings
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
