946f757f584ebddcd7605dd83fdbdddad89ba5be
jing899 <sxw6348@g.rit.edu>
2022-02-23 15:46:53 -0500

    student kit page

--- src/pages/kit/Kit.js
+++ src/pages/kit/Kit.js
@@ -1,15 +1,77 @@
-import React from 'react';
-import NavBar from '../navbar/NavBar';
+import React from "react";
+import NavBar from "../navbar/NavBar";
+import Form from "react-bootstrap/Form";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+
+import { Link } from "react-router-dom";
 
 const Kit = () => {
   return (
     <>
-        <NavBar />
-        <div className='mainContainerRight'>
-
+      <NavBar />
+      <div className="mainContainerRight">
+        <div className="pageTitle">
+          <h3>Kit</h3>
         </div>
+
+        <div class="kitSelection">
+          <Form.Select aria-label="Default select example">
+            <option value="1">Current Kit</option>
+            <option value="2">Past Kit</option>
+            <option value="3">Pending Kit</option>
+          </Form.Select>
+        </div>
+
+        <div class="kitTable">
+          <Table responsive="sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Course</th>
+                <th>Kit Name</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>
+                  <Link to="">
+                    <Button variant="secondary">View Details</Button>{" "}
+                  </Link>
+                </td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>
+                  {" "}
+                  <Link to="">
+                    <Button variant="secondary">View Details</Button>{" "}
+                  </Link>
+                </td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>
+                  {" "}
+                  <Link to="">
+                    <Button variant="secondary">View Details</Button>{" "}
+                  </Link>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default Kit;
