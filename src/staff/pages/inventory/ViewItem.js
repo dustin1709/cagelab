77ce7fe623beefd220e09732a3e524fb460d7820
Valerie <valerie@scapp13.rit.edu>
2022-04-03 17:27:39 -0400

    reservation history complete

--- src/staff/pages/inventory/ViewItem.js
+++ src/staff/pages/inventory/ViewItem.js
@@ -7,10 +7,14 @@ import StaffNavBar from "../../components/StaffNavBar";
 const ViewItem = () => {
   const API_URL = "http://192.168.192.31:3000/item_type/type/all/";
   const API_URL_INST = "http://192.168.192.31:3000/item_instance/type/quantity/";
+  const url_reserved = "http://192.168.192.31:3000/borrower_item/type/quantityReserved/";
+  const url_checkedout = "http://192.168.192.31:3000/borrower_contents/type/quantityCheckedout/";
   const [ myUser, setMyUser ] = useState('');
   const navigate = useNavigate();
   const [ items, setItems ] = useState([]);
   const [ amount, setAmount ] = useState(0);
+  const [ check, setCheck ] = useState(0);
+  const [ reserve, setReserve ] = useState(0);
   const [ borrower, setBorrower ] = useState('');
   const {id} = useParams();
   useEffect(() => {
@@ -24,13 +28,19 @@ const ViewItem = () => {
     const loadTypes = async () => {
       let result = await fetch(API_URL + id);
       let result2 = await fetch(API_URL_INST + id);
+      let reserved = await fetch(url_reserved + id);
+      let checkedout = await fetch(url_checkedout + id);
       console.log("Fetching " + API_URL + "/" + id);
       if(!result.ok) throw Error("Unable to get item types");
       if(!result2.ok) throw Error("Unable to get item instances");
       let res = await result.json();
       let res2 = await result2.json();
+      let resR = await reserved.json();
+      let resC = await checkedout.json();
       console.log(res.item_type);
       setAmount(res2.item_instance[0].total_items);
+      setCheck(resC.borrower_contents[0].amount);
+      setReserve(resR.borrower_item[0].amount);
       setItems(res.item_type);
     }
     loadTypes();
@@ -52,12 +62,18 @@ const ViewItem = () => {
       <StaffNavBar />
       <div className="mainContainerRight">
         <div style={{padding: '3%', margin: '2%'}}>
+        <Link to="/staff/inventory" className="btn btn-primary btn-lg btn-block" style={{padding: '0.5%', marginBottom: '1%'}}>
+          Back to Inventory
+        </Link>
           {items.map((item) => (<h3>{item.model}</h3>))}
-          <h5>Quantity available: {amount}</h5>
+          <h5>Quantity total: {amount}</h5>
+          <h5>Checked out items: {check}</h5>
+          <h5>Reserved items: {reserve}</h5>
+          <h5>Available: {amount - (check + reserve)}</h5>
           <div style={{width: "100%", clear: 'both'}}>
             <h6 style={{padding: '1%'}}>Instance Lending</h6>
             <form onSubmit={add} style={{
-              width: "30%", clear: 'both', padding: '1%', border: '1px solid black', textAlign: 'left'
+              width: "30%", clear: 'both', padding: '1%', textAlign: 'left'
             }}>
                 <label>University ID:</label>
                 <input type="text" 
